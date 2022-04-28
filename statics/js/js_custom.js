eb78ff0ae4ed029813ab80ded58e090978b9dd76
tuancuongbota <93958224+tuancuongbota@users.noreply.github.com>
2022-04-28 14:43:59 +0700

    cuongnt

--- statics/js/js_custom.js
+++ statics/js/js_custom.js
@@ -34,11 +34,55 @@ $(document).ready(function() {
             },
         ],
     });
+    $(".slick-barnd").slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
     $(document).on('mouseenter', '[data-toggle="pill"]', function () {
         $(this).tab('show');
       });
       $('.bota_customers_home .nav-tabs a').on('click',function(e) {
         var bg = $(this).attr('data-bg');
         $('.bota_customers_bg').css('background-image','url('+bg+')');
-      })
+      });
+      $(window).on('load', function () {
+		AOS.refresh();
+	})
+	AOS.init({
+		startEvent: 'load',
+		easing: 'ease-out-back',
+		duration: 1500,
+		once: true
+	});
 });
